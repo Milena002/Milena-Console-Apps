6ccb8a1a5031a7342bc0f61bbc3c5daec854114a
Milena002 <22143664@bucks.ac.uk>
2022-02-11 14:50:58 +0000

    added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,12 +3,47 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    ///This App allows the user to convert distances
+    ///measured in one unit of distance into another 
+    ///unit of distance, for example it will convert 
+    ///a distance measured in miles into the same distance
+    ///measured in feet.
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Milena version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+        /// <summary>
+        /// this public method running the distance converter programm
+        /// </summary>
+        public void Run()
+        {
+
+        }
+        /// <summary>
+        /// this is private method to enter the miles
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+        /// <summary>
+        /// this is private method to change the distance to the feet
+        /// </summary>
+        private void CalculateFeet()
+        {
+
+        }
+        /// <summary>
+        /// this private method displaying the  result in feets
+        /// </summary>
+        private void OutputFeet()
+        {
+
+        }
+
     }
 }
